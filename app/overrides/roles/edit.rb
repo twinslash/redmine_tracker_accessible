a982a4bf9bc82a929c4e3f6b2cada4f7b37406a1
Serge Ahranovich <c.xander@gmail.com>
2014-06-06 19:59:26 +0300

    fix for redmine 2.5.1

--- app/overrides/roles/edit.rb
+++ app/overrides/roles/edit.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   :virtual_path => "roles/edit",
   :name => "tracker_accessible_roles_edit",
-  :insert_after => 'h2',
+  :insert_after => 'erb[silent]',
   :partial => 'roles/tracker_accessible_permission_select_js',
   :disabled => false)
